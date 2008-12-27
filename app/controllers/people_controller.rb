4e85e41df89ad8d3d78bc4ac99b16c7cb83c56d7
Martin Carel <cawel00@yahoo.com>
2008-12-26 22:06:27 -0500

    updated varieties db changed view form override for heler form override updated translations

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -4,7 +4,7 @@ class PeopleController < ResourceController::Base
   create do
     before     :logout_keeping_session!
     after      { self.current_person = @person }
-    flash      "Thanks for signing up!"
+    flash      "Merci de vous être enregistré!"
     wants.html { redirect_back_or_default(root_url) } 
   end
 end
--- app/helpers/wines_helper.rb
+++ app/helpers/wines_helper.rb
@@ -4,4 +4,11 @@ module WinesHelper
     link_to "#{record.reviews.size}", wine_reviews_url(:wine_id => record.id)
   end
   
+  def varieties_form_column(record, input_name)
+    collection_select :record, :variety_ids,
+        Variety.find(:all), :id, :name, {},
+        :multiple => true,
+        :name => 'record[variety_ids][]', :size => 15
+  end
+  
 end
