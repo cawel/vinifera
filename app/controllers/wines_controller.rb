44dec29c707aa820931f4b998b24e6e5033a091e
Martin Carel <cawel00@yahoo.com>
2009-01-30 19:32:45 -0500

    reverting to basic scaffolding with using r_c

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,5 +1,9 @@
 class WinesController < ApplicationController
   layout 'application'
-
+  resource_controller
+  
+  create.response do |wants|
+    wants.html{redirect_to wines_url}
+  end
   
 end
