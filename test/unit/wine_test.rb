73ebf259fbedb55c6ae5f15e8636634a43925a80
Martin Carel <cawel00@yahoo.com>
2009-02-11 20:27:23 -0500

    added test for wine model

--- test/unit/wine_test.rb
+++ test/unit/wine_test.rb
@@ -11,7 +11,8 @@ class WineTest < ActiveSupport::TestCase
   
   should_belong_to :person
   should_belong_to :color
-
+  should_belong_to :country
+  
   should_validate_presence_of :name, :color_id, :person_id
   
   should_have_many :reviews, :dependent => :destroy
