2d1c20cc61d93cbbb560b81bb88be09a5e5ef0ed
Martin Carel <cawel00@yahoo.com>
2009-02-12 23:43:46 -0500

    adding fr translations for rails

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,4 +3,8 @@
 #add overrides for resource_controller
 I18n.load_path += Dir[ File.join(RAILS_ROOT, 'config', 'locales', 'resource_controller', '*.yml') ]
 
+#add overrides for rails
+I18n.load_path += Dir[ File.join(RAILS_ROOT, 'config', 'locales', 'rails', '*.yml') ]
+
+
 I18n.default_locale = "fr-CA"
