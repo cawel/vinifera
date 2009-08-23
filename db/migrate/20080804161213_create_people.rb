850f9ef138b93d21fd137b7b4279657b53bc0a18
Martin Carel <martin.carel@gmail.com>
2009-08-23 17:16:43 -0400

    removed indentation

--- db/migrate/20080804161213_create_people.rb
+++ db/migrate/20080804161213_create_people.rb
@@ -10,8 +10,6 @@ class CreatePeople < ActiveRecord::Migration
       t.column :updated_at,                :datetime
       t.column :remember_token,            :string, :limit => 40
       t.column :remember_token_expires_at, :datetime
-
-
     end
     add_index :people, :login, :unique => true
   end
