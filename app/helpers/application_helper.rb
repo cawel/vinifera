f4d53ebd160e42f43f75efb95f1413599f24c31b
Martin Carel <martin.carel@gmail.com>
2009-09-10 21:58:49 -0400

    added stars rating helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  
+
   def editable_wine?(wine, &block)
     if current_person.andand.admin || current_person == wine.person
       block.call
@@ -8,7 +8,7 @@ module ApplicationHelper
       "&nbsp;"
     end
   end
-  
+
   def editable_review?(review, &block)
     if (review.person == current_person) || current_person.andand.admin
       block.call
@@ -16,25 +16,36 @@ module ApplicationHelper
       "&nbsp;"
     end
   end
-  
+
   def person_display_name person
     if person.nil?
       return "[no user name]"
     end
     person.name.blank? ? person.email : person.name
   end
-  
+
   def format_review_comment text
     simple_format word_wrap((text))
   end
-  
+
   def country_flag country
     image_path = get_flag_image_path(country)
     image_path.blank?? '' : "<img class='flag' title='#{country.name}' src=\"#{image_path}\"/>"   
   end 
-  
+
   def field_exists? field
     yield unless field.nil?
+  end
+
+  def stars_rating stars
+    case stars
+    when "*" ; image_tag("stars/1star.gif")
+    when "**";  image_tag("stars/2star.gif")
+    when "***";  image_tag("stars/3star.gif")
+    when "****";  image_tag("stars/4star.gif")
+    when "*****";  image_tag("stars/5star.gif")
+    end
+
   end
 
   private
@@ -42,5 +53,5 @@ module ApplicationHelper
     image_path = "/images/flags/" + country.code.downcase + ".gif"
     File.exists?("#{RAILS_ROOT}/public/#{image_path}") ? image_path : nil
   end
-  
+
 end
