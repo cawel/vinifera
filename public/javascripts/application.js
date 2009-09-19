7637905250a070a0c9e49a2fc614a01d1a512254
fsbrainstorm <fsbrainstorm@gmail.com>
2009-09-18 21:16:48 -0400

    Added tentative star rating system.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+
+  // Star Ratings Behaviour
+  $("ul.star_rating li a").click(function() {
+    $("#rating").val($(this).parent().attr("class").replace("stars_", "").replace("current", "").replace(" ", ""));
+    $("ul.star_rating li").removeClass("current");
+    $(this).parent().addClass("current");
+  });
+
+});
