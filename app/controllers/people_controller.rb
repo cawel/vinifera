d5794279d19c7b62ed79025b9cb79b0757216154
Martin Carel <martin.carel@gmail.com>
2009-09-26 14:38:37 -0400

    cleaned I18n files

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,18 +3,14 @@ class PeopleController < ResourceController::Base
   before_filter :login_required, :only => [:edit, :update]
   before_filter :prevent_inpersonification, :only => [:edit, :update]
   
-  before_filter :localizate
-  
-  def localizate
-    I18n.locale = params[:locale] || I18n.default_locale
-  end
-  
   create do
     before     :logout_keeping_session!
     after      { self.current_person = @person }
     flash      I18n.t(:signup_complete)
     wants.html { redirect_back_or_default(root_url) } 
   end
+  
+  update.flash "Votre profil a été mis a jour!"
   
   def prevent_inpersonification
     if current_person.andand != Person.find_by_id(params[:id])
