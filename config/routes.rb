912b8a090e4db8a6e65c3f11e684dcf154eb5afa
Martin Carel <martin.carel@gmail.com>
2009-09-27 11:09:54 -0400

    made French url's

--- config/routes.rb
+++ config/routes.rb
@@ -8,29 +8,29 @@ ActionController::Routing::Routes.draw do |map|
   end
   
   # frontend
-  map.resources :wines do |wine|
-    wine.resources :reviews
+  map.resources :wines, :as => "vins" do |wine|
+    wine.resources :reviews, :as => "critiques"
   end
   
-  map.root                   :controller => 'wines', :action => 'home'
-  map.search    '/search',   :controller => 'wines', :action => 'index'
+  map.root                                      :controller => 'wines',     :action => 'home'
+  map.search        '/resultats-de-recherche',  :controller => 'wines',     :action => 'index'
 
-  map.about         '/a-propos',    :controller => 'contents', :action => 'about'
-  map.contact       '/contact',  :controller => 'contents', :action => 'contact'
-  map.ratings_help  '/signification-des-cotes',  :controller => 'contents', :action => 'ratings_help'
+  map.about         '/a-propos',                :controller => 'contents',  :action => 'about'
+  map.contact       '/contact',                 :controller => 'contents',  :action => 'contact'
+  map.ratings_help  '/signification-des-cotes', :controller => 'contents',  :action => 'ratings_help'
   
-  map.login     '/login',    :controller => 'sessions',    :action => 'new'
-  map.logout    '/logout',   :controller => 'sessions',    :action => 'destroy'
+  map.login         '/ouvrir-session',          :controller => 'sessions',  :action => 'new'
+  map.logout        '/fermer-session',          :controller => 'sessions',  :action => 'destroy'
   
-  map.register  '/register', :controller => 'people',     :action => 'create'
-  map.signup    '/signup',   :controller => 'people',     :action => 'new'
+  map.register      '/enregistrer',             :controller => 'people',    :action => 'create'
+  map.signup        '/creer-profil',            :controller => 'people',    :action => 'new'
   
-  map.reset_password '/reset_password', :controller => 'password_resets', :action => 'new',    :conditions => {:method => :get}
-  map.reset_password '/reset_password', :controller => 'password_resets', :action => 'create', :conditions => {:method => :post}
+  # map.reset_password '/reset_password',         :controller => 'password_resets', :action => 'new',    :conditions => {:method => :get}
+  # map.reset_password '/reset_password',         :controller => 'password_resets', :action => 'create', :conditions => {:method => :post}
   
   map.connect '/account', :controller => 'accounts', :action => 'edit'
   
-  map.resources :people, :as => 'usager' do |person|
+  map.resources :people, :as => 'profil' do |person|
     person.reviews_index 'critiques', :controller => 'reviews', :action => 'user_index'
   end
 
