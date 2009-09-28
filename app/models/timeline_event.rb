4f55d2f8c8f11f34f10c5c13cce4a67ea3b50b7d
Martin Carel <martin.carel@gmail.com>
2009-09-27 20:56:06 -0400

    reduced nb items in activity stream

--- app/models/timeline_event.rb
+++ app/models/timeline_event.rb
@@ -3,5 +3,5 @@ class TimelineEvent < ActiveRecord::Base
   belongs_to :subject,            :polymorphic => true
   belongs_to :secondary_subject,  :polymorphic => true
 
-  named_scope :recent, :order => "created_at desc limit 10"
+  named_scope :recent, :order => "created_at desc limit 7"
 end
