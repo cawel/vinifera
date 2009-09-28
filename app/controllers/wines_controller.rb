dd6bea74c9c6abc06e4f44e7d45d8645796a5c39
Martin Carel <martin.carel@gmail.com>
2009-09-27 21:38:33 -0400

    cleaned code

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,12 +1,9 @@
 class WinesController < ApplicationController
+  actions :index, :show
   before_filter :login_required, :except => [:home, :index, :show]
 
   layout 'application'
   resource_controller
-
-  def home
-    @timeline_events = TimelineEvent.recent
-  end
 
   index.response do |wants|
     wants.html do 
@@ -17,21 +14,5 @@ class WinesController < ApplicationController
       end
     end
   end
-
-  create.before do
-    object.person = current_person
-  end
-
-  create.flash "Votre vin a été ajouté!"
-  create.response do |wants|
-    wants.html{redirect_to wine_reviews_url(@wine)}
-  end
-
-  update.flash {"Votre vin a été mis à jour!"}
-  update.response do |wants|
-    wants.html{redirect_to wine_reviews_url(@wine)}
-  end
-
-  destroy.flash {"Votre vin a été retiré!"}
 
 end
