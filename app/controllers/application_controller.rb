9f5ed7088006344deb6af77fa2c5378b0d2c3be4
Martin Carel <martin.carel@gmail.com>
2009-09-27 23:54:01 -0400

    nicely fixed exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   helper :all
   before_filter :configure_mailers
   before_filter :localizate
-  before_filter :top_contributers
   before_filter :top_wines
+  before_filter :top_contributers
   
   def localizate
     #I18n.locale = params[:locale] || I18n.default_locale
@@ -23,6 +23,19 @@ class ApplicationController < ActionController::Base
 
   def top_wines
     @top_wines = Wine.top_wines
+  end
+
+  def local_request?
+    false
+  end
+
+  def rescue_action_in_public(exception)
+    if exception.is_a? ActionController::RoutingError 
+      render :template => "/layouts/404.html.erb", :layout => true, :status => 404
+    else
+      render :template => "/layouts/500.html.erb", :layout => true, :status => 500
+    end
+    notify_hoptoad(exception)
   end
 
   # See ActionController::Base for details 
