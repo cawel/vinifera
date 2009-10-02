1baef4408b22eede6ff5a11cc1a9ccb7e3593369
Martin Carel <martin.carel@gmail.com>
2009-10-01 23:00:18 -0400

    changed default format for homepage (IE bug)

--- app/controllers/timeline_events_controller.rb
+++ app/controllers/timeline_events_controller.rb
@@ -3,10 +3,10 @@ class TimelineEventsController < ApplicationController
   def index
     respond_to do |format|
       @timeline_events = TimelineEvent.recent
+      format.html
       format.rss do  
         render :layout => false 
       end 
-      format.html
     end 
   end
 
