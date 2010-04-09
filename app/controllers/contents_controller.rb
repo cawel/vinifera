96d7b74080619cf7a011b13a4026814dd44a3b10
Martin Carel <martin.carel@gmail.com>
2010-04-08 21:12:32 -0400

    added nouvelles du vin

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,5 +1,8 @@
 class ContentsController < ApplicationController
 
+  require 'simple-rss'
+  require 'open-uri'
+
   def contact
   end
 
@@ -8,4 +11,10 @@ class ContentsController < ApplicationController
 
   def ratings_help
   end
+
+  def feed
+    url = "http://pipes.yahoo.com/pipes/pipe.run?_id=0183129386d42d4a011bd5692f8f63f4&_render=rss"
+    @feed = SimpleRSS.parse open(url)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ ActionController::Routing::Routes.draw do |map|
   map.about         '/a-propos',                :controller => 'contents',        :action => 'about'
   map.contact       '/contact',                 :controller => 'contents',        :action => 'contact'
   map.ratings_help  '/signification-des-cotes', :controller => 'contents',        :action => 'ratings_help'
+  map.nouvelles     '/nouvelles-monde-du-vin', :controller => 'contents',        :action => 'feed'
   
   map.login         '/ouvrir-session',          :controller => 'sessions',        :action => 'new'
   map.logout        '/terminer-session',        :controller => 'sessions',        :action => 'destroy'
