76b81b7d6ad5689032254e8b89da4fe1aedd75b0
Martin Carel <martin.carel@gmail.com>
2010-04-25 21:57:32 -0400

    addd splash + minor CSS tweaks

--- app/models/timeline_event.rb
+++ app/models/timeline_event.rb
@@ -3,5 +3,5 @@ class TimelineEvent < ActiveRecord::Base
   belongs_to :subject,            :polymorphic => true
   belongs_to :secondary_subject,  :polymorphic => true
 
-  named_scope :recent, :order => "created_at desc limit 7"
+  named_scope :recent, :order => "created_at desc limit 5"
 end
