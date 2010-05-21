eb6b60d5c5ed67733174a2ee477c7655842abf40
Martin Carel <martin.carel@gmail.com>
2010-05-21 00:44:36 -0400

    added monitoring

--- script/fetch_images.rb
+++ script/fetch_images.rb
@@ -1,13 +1,16 @@
 require 'ruby-debug'
 
 images_fetched = 0
+count = Wine.count
 
-Wine.all.each do |wine|
-  unless File.exists?("public/images/bottles/#{wine.code_saq}.jpg")
+Wine.all.each_with_index do |wine, index|
+  unless File.exists?("public/images/bottles/#{wine.code_saq}.jpg") || File.exists?("public/images/bottles/new/#{wine.code_saq}.jpg")
     puts "fetching #{wine.code_saq}..."
     `curl -L "http://www.saq.com/wcsstore/saqcom/images_produits/#{wine.code_saq}_g.jpg" > public/images/bottles/new/#{wine.code_saq}.jpg`
     images_fetched += 1
   end
+  sleep 0.1
+  puts "#{index} / #{count}"
 end
 
 puts "images fetched: #{images_fetched}"
