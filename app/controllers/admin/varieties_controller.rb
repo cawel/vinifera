aec4e6f0d17bf1e81477dee2b822a762faad2fd0
Martin Carel <martin.carel@gmail.com>
2010-06-08 20:37:30 -0400

    commented out active_scaffold

--- app/controllers/admin/varieties_controller.rb
+++ app/controllers/admin/varieties_controller.rb
@@ -1,10 +1,10 @@
 class Admin::VarietiesController < Admin::ApplicationController
   layout 'application'
   
-  active_scaffold :variety do |config|
-    config.columns = [:name]
-    list.sorting = {:name => 'ASC'}
-  end
+  #active_scaffold :variety do |config|
+    #config.columns = [:name]
+    #list.sorting = {:name => 'ASC'}
+  #end
   
   
 end
