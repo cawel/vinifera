cf6d4c86b6c0499edf5bac625aebe40a880cf237
Martin Carel <martin.carel@gmail.com>
2011-11-23 17:39:05 -0500

    fixed deprecation warning: config.load_paths to config.autoload_paths

--- config/environment.rb
+++ config/environment.rb
@@ -47,7 +47,7 @@ Rails::Initializer.run do |config|
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Add additional load paths for your own custom dirs
-  config.load_paths += %W( #{RAILS_ROOT}/app/presenters #{RAILS_ROOT}/app/mailers )
+  config.autoload_paths += %W( #{RAILS_ROOT}/app/presenters #{RAILS_ROOT}/app/mailers )
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)
